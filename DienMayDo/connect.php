a01d9f4253f730ad09bc0360542d5ac457a1b046
NguyenPhuVinh26 <91648114+NguyenPhuVinh26@users.noreply.github.com>
2021-10-22 19:53:51 +0700

    Update connect.php

--- DienMayDo/connect.php
+++ DienMayDo/connect.php
@@ -4,8 +4,8 @@
 	mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);*/
 	$Connect = pg_connect("postgres://jmdrhuyxeteupz:59d17582c5cf3f93d4c70701169649492bf1ca1c26e7be4c1fed36ee1c91e358@ec2-34-205-14-168.compute-1.amazonaws.com:5432/d2hk0ut4ifpeo7");
-    //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
-	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
+    //$Connect = pg_connect("host=localhost port=5432 dbname=ec2-3-212-168-103.compute-1.amazonaws.com");
+	//$Connect = pg_connect("host=localhost port=5432 dbname=ec2-3-212-168-103.compute-1.amazonaws.com user=oshceostldedzh password=123456");
 	
     if (!$Connect) {
         die("Connection failed");
